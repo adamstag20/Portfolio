ae1e6e60c40b4fd5a9c3ddcd76394334201ca63d
Adam Stagner <stagner2@pdx.edu>
2023-03-10 19:50:33 -0800

    Created Contact Form using JS

---
+++ contact.js
@@ -0,0 +1,50 @@
+
+function submitForm() {
+    const name = document.getElementById("name");
+    console.log("Name: " + name.value);
+
+    const email = document.getElementById("email");
+    console.log("Email: " + email.value);
+    
+    const message = document.getElementById("message");
+    console.log("Message: " + email.value);
+}
+
+
+const contactPage = document.querySelector(".contact");
+contactPage.style.marginRight = "auto";
+contactPage.style.marginLeft = "auto";
+contactPage.style.marginTop = "150px";
+contactPage.style.width = "50%";
+contactPage.style.backgroundColor = "rgb(12, 14, 39)";
+contactPage.style.paddingLeft = "100px";
+contactPage.style.paddingRight = "100px";
+contactPage.style.paddingTop = "30px";
+contactPage.style.paddingBottom = "100px";
+
+const form = document.querySelector("form");
+form.style.textAlign = "center";
+
+
+const header = document.querySelector(".contact-header");
+header.style.paddingTop = "0px";
+header.style.paddingLeft = "0px"
+header.style.color = "white";
+header.style.textAlign = "center";
+
+const name = document.getElementById("name");
+    name.style.fontFamily = "Roboto, serif";
+    name.style.width = "400px";
+    name.style.height = "25px";
+    name.style.alignContent = "center";
+
+const email = document.getElementById("email");
+    email.style.fontFamily = "Roboto, serif";
+    email.style.width = "400px";
+    email.style.height = "25px";
+
+const message = document.getElementById("message");
+    message.style.fontFamily = "Roboto, serif";
+    message.style.width = "400px";
+    message.style.paddingBottom = "75px";
+    message.style.fontSize = "10px";
