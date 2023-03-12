68e18961a8a4092b48163176d07a292ab321e919
Adam Stagner <stagner2@pdx.edu>
2023-03-11 18:43:29 -0800

    Created projects page. Need to do accessibility checks and fix minor issues

--- contact.js
+++ contact.js
@@ -1,15 +1,13 @@
-
 function submitForm() {
-    const name = document.getElementById("name");
-    console.log("Name: " + name.value);
+  const name = document.getElementById("name");
+  console.log("Name: " + name.value);
 
-    const email = document.getElementById("email");
-    console.log("Email: " + email.value);
-    
-    const message = document.getElementById("message");
-    console.log("Message: " + email.value);
+  const email = document.getElementById("email");
+  console.log("Email: " + email.value);
+
+  const message = document.getElementById("message");
+  console.log("Message: " + email.value);
 }
-
 
 const contactPage = document.querySelector(".contact");
 contactPage.style.marginRight = "auto";
@@ -25,26 +23,25 @@ contactPage.style.paddingBottom = "100px";
 const form = document.querySelector("form");
 form.style.textAlign = "center";
 
-
 const header = document.querySelector(".contact-header");
 header.style.paddingTop = "0px";
-header.style.paddingLeft = "0px"
+header.style.paddingLeft = "0px";
 header.style.color = "white";
 header.style.textAlign = "center";
 
 const name = document.getElementById("name");
-    name.style.fontFamily = "Roboto, serif";
-    name.style.width = "400px";
-    name.style.height = "25px";
-    name.style.alignContent = "center";
+name.style.fontFamily = "Roboto, serif";
+name.style.width = "400px";
+name.style.height = "25px";
+name.style.alignContent = "center";
 
 const email = document.getElementById("email");
-    email.style.fontFamily = "Roboto, serif";
-    email.style.width = "400px";
-    email.style.height = "25px";
+email.style.fontFamily = "Roboto, serif";
+email.style.width = "400px";
+email.style.height = "25px";
 
 const message = document.getElementById("message");
-    message.style.fontFamily = "Roboto, serif";
-    message.style.width = "400px";
-    message.style.paddingBottom = "75px";
-    message.style.fontSize = "10px";
+message.style.fontFamily = "Roboto, serif";
+message.style.width = "400px";
+message.style.paddingBottom = "75px";
+message.style.fontSize = "10px";
